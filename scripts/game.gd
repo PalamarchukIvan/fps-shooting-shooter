ed59e33bcbd2b6801f8b367f5a42a1ba7afdcb5f
Ivan <vanavana978@gmail.com>
2025-07-19 17:58:41 +0300

    Fisrt commit

---
+++ scripts/game.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+func _ready() -> void:
+	print("Hello world!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
