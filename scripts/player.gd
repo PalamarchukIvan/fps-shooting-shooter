991657275de719e1f46c1af52f8a8790b9cc4384
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2025-07-20 19:40:54 +0300

    reworked jumping by introducing grav_pow variable

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 # to be exported and managed externaly
 @export var look_speed : float = 0.002
 @export var move_speed : float = 10
-@export var jump_speed : float = 7
+@export var jump_speed : float = 10
 
 @export var input_left : String = "ui_left"
 @export var input_right : String = "ui_right"
@@ -19,6 +19,7 @@ extends CharacterBody3D
 
 # properties 
 var look_rotation : Vector2
+var grav_pow: float = 1 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,6 +30,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_character(event.relative)
 
+func _physics_process(delta: float) -> void:
+	move_character(delta)
+
 func rotate_character(rot_input : Vector2) -> void: 
 	look_rotation.x -= rot_input.x * look_speed
 	look_rotation.y -= rot_input.y * look_speed
@@ -38,8 +42,9 @@ func rotate_character(rot_input : Vector2) -> void:
 	
 	rotation.y = look_rotation.x  # rotate CharacterBody3D by y-axis > | y  
 	camera.rotation.x = look_rotation.y  # rotate Camera by x-axis ^ --- x 
-
-func _physics_process(delta: float) -> void:
+	
+func move_character(delta: float) -> void: 
+	# movement handling 
 	var input_dir_2d := Input.get_vector(
 		input_left, input_right, input_forward, input_back
 	)
@@ -51,14 +56,25 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction.x * move_speed
 	velocity.z = direction.z * move_speed
 	
-	velocity += get_gravity() * delta
+	# jump handling 
 	if Input.is_action_just_pressed(input_jump) and is_on_floor():
 		velocity.y = jump_speed
+	elif Input.is_action_just_released(input_jump) and velocity.y > 0: 
+		velocity.y = 0
+		
+	# if player is falling => increase grav power
+	if velocity.y < 0: 
+		print(velocity)
+		grav_pow = 2
+	else: 
+		grav_pow = 1
+		 
+	velocity += get_gravity() * grav_pow * delta 
 	
+	# apply position changes based on velocity 
 	move_and_slide()
 	
 	if (is_debug_enabled):
 		print("DEBUG: velocity: ", velocity)
 		print("DEBUG: position: ", global_position)
 		print("DEBUG: basis: ", basis)
-		pass 
